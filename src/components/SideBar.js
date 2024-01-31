6e0f8cb623d02ae3244c4fc4b373a57f15aa9b57
saravana1405 <saran14052003@gmail.com>
2024-01-31 14:22:50 +0530

    some styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,24 +12,24 @@ const SideBar = () => {
       {['md'].map((expand) => (
         <Navbar key={expand} expand={expand} className="bg-dark mb-3">
           <Container>
-            <Navbar.Brand href="#" className='text-white'><FaShopify className='text-warning h1' id='shop'/><span className='h2 text-tertiary px-2'>SK</span> <span className='lead fs-6'>Shoping</span></Navbar.Brand>
+            <Navbar.Brand href="#" className='text-white'><FaShopify className='h1' id='shop'/><span className='h3 text-tertiary heading'>SK</span> <span className='lead fs-6' id='shoping'>Shoping</span></Navbar.Brand>
             <Navbar.Toggle className='button' aria-controls={`offcanvasNavbar-expand-${expand}`} ><FaBars className='bar fs-2'/></Navbar.Toggle>
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
             >
-              <Offcanvas.Header  closeButton>
-                <Offcanvas.Title className='text-primary' id={`offcanvasNavbarLabel-expand-${expand}`}>
+              <Offcanvas.Header  closeButton id='back'>
+                <Offcanvas.Title className='text-dark' id={`offcanvasNavbarLabel-expand-${expand}`}>
                  
                 <FaHome className='m-1 text-dark'/> Menu
                 </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
+              <Offcanvas.Body id='back2'>
                 <Nav className="justify-content-end flex-grow-1 pe-4 p-2 mx-5">
-                  <Nav.Link href="#action1" className='text-warning link'>Home</Nav.Link>
-                  <Nav.Link href="#action2" className='text-warning  link'>About</Nav.Link>
-                  <Nav.Link href="#action2" className='text-warning link'>Catagories</Nav.Link>
+                  <Nav.Link href="#action1" id='link'>Home</Nav.Link>
+                  <Nav.Link href="#action2" id='link2'>About</Nav.Link>
+                  <Nav.Link href="#action2" id='link3'>Catagories</Nav.Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
@@ -40,7 +40,7 @@ const SideBar = () => {
                     className="me-3"
                     aria-label="Search"
                   />
-                  <Button variant="btn btn-warning">Search</Button>
+                  <Button variant="btn" id='button'>Search</Button>
                 </Form>
                 
           </Container>
